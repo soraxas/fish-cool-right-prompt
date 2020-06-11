fc072f008dce69f7b9f2c03a44c171ebb0123a04
Tin Lai <oscar@tinyiu.com>
2020-06-11 22:13:31 +1000

    init, with pipe status and ranger level

---
+++ fish_right_prompt.fish
@@ -0,0 +1,32 @@
+
+# copy the current fish prompt, and wrap it to save pipe status
+functions -c fish_prompt __original_fish_prompt
+# we limits outself on what we perform in this wrapped fish_prompt.
+# we won't modify $stats as we are only performing set (i.e. won't affect most prompt).
+# However, `set` would modify $pipestatus though
+# (see github.com/fish-shell/fish-shell/issue/6815)
+function fish_prompt
+  set -g last_pipestatus $pipestatus
+  __original_fish_prompt $argv
+end
+
+function fish_right_prompt
+  # print ranger level with our helper function
+  __print_ranger_level
+  if test -z "$last_pipestatus"
+    # pipe status not set!
+    # Check if you have this at the beginning of the normal prompt
+    # set -g last_pipestatus $pipestatus
+    set_color red
+    echo Invalid setup!
+    set_color normal
+
+  else
+    # print pipe status.
+    # taken from official fish prompt
+    __fish_print_pipestatus "[" "]" "|" (set_color $fish_color_status) \
+        (set_color --bold $fish_color_status) $last_pipestatus
+    # erase it so we know if it is properly set next time
+    set -e last_pipestatus
+  end
+end
---
+++ functions/__print_ranger_level.fish
@@ -0,0 +1,32 @@
+function __print_ranger_level
+  # Ranger indicator
+  # ₀₁₂₃₄₅₆₇₈₉₊₋₌₍₎ₐₑₒₓ
+  # ⁰¹²³⁴⁵⁶⁷⁸⁹⁺⁻⁼⁽⁾ⁿⁱ
+  if test -n "$RANGER_LEVEL"
+    # orange and white, from ranger's logo color
+    # we will use no background and orange font
+    set_color --bold df661c
+
+    # commented is orange background and white font
+    #set_color -b df661c; set_color --bold ebebeb
+
+    echo R
+    switch $RANGER_LEVEL
+      case '1'
+        echo ¹
+      case '2'
+        echo ²
+      case '3'
+        echo ³
+      case '4'
+        echo ⁴
+      case '5'
+        echo ⁵
+      case '*'
+      echo ⁵⁺
+    end
+    set_color normal
+
+    echo " "
+  end
+end
